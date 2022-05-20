f2d37932db6e2a1cb0882741e9e205345a5accb7
Rob Trehy <robert.trehy@outlook.com>
2022-05-20 11:09:32 +0100

    Updated z-index

--- src/Components/ConfirmDeleteModal.js
+++ src/Components/ConfirmDeleteModal.js
@@ -13,7 +13,7 @@ const ConfirmDeleteModal = props => {
     } = props;
 
     return (
-        <div className="fixed z-50 inset-0 overflow-y-auto" {...attributes}>
+        <div className="fixed z-[500] inset-0 overflow-y-auto" {...attributes}>
             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
                 <div className="fixed inset-0 transition-opacity" aria-hidden="true">
