c25221f978d9d075f903a0f3cb96aa322b355070
Robert Trehy <robert.trehy@outlook.com>
2022-07-07 14:16:03 +0100

    Make HeaderBar shorter

--- src/Components/HeaderBar.js
+++ src/Components/HeaderBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Headerbar = ({ children, ...rest }) => {
     return (
-        <div className="relative flex flex-0 items-center w-full h-16 min-h-[4rem] px-4 md:px-6 shadow dark:shadow-none dark:border-b dark:border-gray-800 bg-white dark:bg-transparent z-[190]" {...rest}>
+        <div className="relative flex flex-0 items-center w-full h-14 min-h-[3.5rem] px-4 md:px-6 shadow dark:shadow-none dark:border-b dark:border-gray-800 bg-white dark:bg-transparent z-[190]" {...rest}>
             {children}
         </div>
     );
